4807a932be8d97a8e552a63b3adcd7de747534cd
ouss-issib <oussamaissib@gmail.com>
2025-05-04 15:44:05 +0100

    feat : Implementation code for The Entity Product

---
+++ src/main/java/ma/enset/springmvcthymeleafsecurityapp/entities/Product.java
@@ -0,0 +1,25 @@
+package ma.enset.springmvcthymeleafsecurityapp.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter@Setter@ToString@Builder
+
+public class Product {
+    @Id @GeneratedValue
+    private Long id;
+    @Size(min = 2, max = 50)
+    private String name;
+    @Min(0)
+    private double price;
+    @Min(1)
+    private int quantity;
+}
