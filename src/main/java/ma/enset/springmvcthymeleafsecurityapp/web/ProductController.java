6fe735ead715b3348c1dd1b9b2a2c05e370044af
ouss-issib <oussamaissib@gmail.com>
2025-05-04 17:35:08 +0100

    feat : complete implementation for adding new product

--- src/main/java/ma/enset/springmvcthymeleafsecurityapp/web/ProductController.java
+++ src/main/java/ma/enset/springmvcthymeleafsecurityapp/web/ProductController.java
@@ -1,12 +1,15 @@
 package ma.enset.springmvcthymeleafsecurityapp.web;
 
+import jakarta.validation.Valid;
 import ma.enset.springmvcthymeleafsecurityapp.entities.Product;
 import ma.enset.springmvcthymeleafsecurityapp.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -15,7 +18,13 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
+
     @GetMapping("/")
+    public String home() {
+        return "redirect:/index";
+    }
+
+    @GetMapping("/index")
     public String index(Model model) {
         model.addAttribute("productList" ,productRepository.findAll());
         return "products";
@@ -26,5 +35,21 @@ public class ProductController {
     public String delete(@RequestParam(name = "id") Long id) {
         productRepository.deleteById(id);
         return "redirect:/";
+    }
+
+    @GetMapping("/newProduct")
+    public String newProduct(Model model){
+        model.addAttribute("product", new Product());
+        return "new-product";
+    }
+
+
+    @PostMapping("/saveProduct")
+    public String saveProduct(@Valid Product product, BindingResult bindingResult,Model model) {
+        if(bindingResult.hasErrors()) {
+            return "new-product";
+        }
+        productRepository.save(product);
+        return "redirect:/index";
     }
 }
