b40614cb44511f83aac1829833aab73785d5e658
ouss-issib <oussamaissib@gmail.com>
2025-05-04 16:20:07 +0100

    feat : add products page in templates

--- src/main/java/ma/enset/springmvcthymeleafsecurityapp/web/ProductController.java
+++ src/main/java/ma/enset/springmvcthymeleafsecurityapp/web/ProductController.java
@@ -1,13 +1,20 @@
 package ma.enset.springmvcthymeleafsecurityapp.web;
 
+import ma.enset.springmvcthymeleafsecurityapp.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProductController {
 
+    @Autowired
+    ProductRepository productRepository;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("productList" ,productRepository.findAll());
         return "products";
     }
 }
