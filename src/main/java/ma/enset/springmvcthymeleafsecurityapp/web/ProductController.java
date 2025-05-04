6b50efe5de647661bc584f15cac41770c45940c2
ouss-issib <oussamaissib@gmail.com>
2025-05-04 16:44:41 +0100

    chore : add theme leaf layout dialect dependency

--- src/main/java/ma/enset/springmvcthymeleafsecurityapp/web/ProductController.java
+++ src/main/java/ma/enset/springmvcthymeleafsecurityapp/web/ProductController.java
@@ -1,10 +1,13 @@
 package ma.enset.springmvcthymeleafsecurityapp.web;
 
+import ma.enset.springmvcthymeleafsecurityapp.entities.Product;
 import ma.enset.springmvcthymeleafsecurityapp.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProductController {
@@ -16,5 +19,12 @@ public class ProductController {
     public String index(Model model) {
         model.addAttribute("productList" ,productRepository.findAll());
         return "products";
+    }
+
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam(name = "id") Long id) {
+        productRepository.deleteById(id);
+        return "redirect:/";
     }
 }
