754b114d1c921d7707d6fff05ba9213bd9168951
ouss-issib <oussamaissib@gmail.com>
2025-05-04 15:35:21 +0100

    conf: initialise the application environment

---
+++ src/main/java/ma/enset/springmvcthymeleafsecurityapp/SpringMvcThymeleafSecurityAppApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.springmvcthymeleafsecurityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcThymeleafSecurityAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcThymeleafSecurityAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/springmvcthymeleafsecurityapp/SpringMvcThymeleafSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.springmvcthymeleafsecurityapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcThymeleafSecurityAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
